192c89f4a8e54ae9ea9af8f519a15c542a17b94d
Pascual <pascual.munoz@pccomponentes.com>
2018-04-22 11:53:16 +0200

    test y stubs listos

--- __tests__/n2h.test.js
+++ __tests__/n2h.test.js
@@ -1,5 +1,16 @@
+/**
+ * @type {n2h}
+ */
 const n2h = require('../src/n2h');
 
-test('si le meto 13456745', function () {
+test('si le meto enorme como el 13456745 lo hace bien', function () {
     expect(n2h(13456745)).toBe('trece millones cuatrocientos cincuenta y siete mil setecientos cuarenta y cinco');
+});
+
+test('Si el primer dígito es un 1 en un número de más de una terna, entonces no tengo que decir "uno mil"', function () {
+  expect(n2h(1001)).not.toBe('uno mil uno');
+});
+
+test('hago bien el 1001', function () {
+    expect(n2h(1001)).toBe('mil uno');
 });
--- src/terna2h.js
+++ src/terna2h.js
@@ -6,5 +6,9 @@
 function terna2h(terna) {
     if ( terna === 745) return 'setecientos cuarenta y cinco';
     if ( terna === 27) return 'veintisiete';
+    if ( terna === 16) return 'dieciseis';
+    if ( terna === 17) return 'diecisiete';
+    if ( terna === 18) return 'dieciocho';
+    if ( terna === 19) return 'diecinueve';
 }
 module.exports = terna2h;
