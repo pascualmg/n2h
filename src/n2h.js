7b25d0cebe3482a10357a4e3667462695a6bdcc0
Pascual <pascual.munoz@pccomponentes.com>
2018-04-28 12:03:36 +0200

    añadida documentación util de las propiedades numéricas. eliminada de la logica de la recursiva la traducción del peso. esta se ha migrado internamente a los traductores haciendo que reciban un parámetro más , ( el peso actual ) ya que serán los encargados de controlar las excepciones linguísticas.

--- src/n2h.js
+++ src/n2h.js
@@ -1,13 +1,19 @@
 const periodTranslator = require('./periodTranslator.js');
 
-// https://es.wikipedia.org/wiki/Anexo:Nombres_de_los_n%C3%BAmeros_en_espa%C3%B1ol
-// https://wikimedia.org/api/rest_v1/media/math/render/svg/6e2fb44a6df602e5c9522edf731d3889cd6c208b
+
 function n2h(numberStr) {
     const periodLenght = 6;
     const initialWeight = 0;
-    return parseRecursively (numberStr, periodLenght, initialWeight, periodTranslator, blockWeightTranslator).trim();
+    return parseRecursively (numberStr, periodLenght, initialWeight, periodTranslator).trim();
 }
-
+/**
+ * https://es.wikipedia.org/wiki/Anexo:Nombres_de_los_n%C3%BAmeros_en_espa%C3%B1ol
+ * https://wikimedia.org/api/rest_v1/media/math/render/svg/6e2fb44a6df602e5c9522edf731d3889cd6c208b
+ * Nomenclatura standard de los blocks según la wikipedia.
+ * si un block es una string que tiene un tamaño fijo de 6 caracteres es un period.
+ * si un block es una string que tiene un tamaño fijo de 3 caracteres es una terna.
+ * si un block es una string que tiene un tamaño fijo de 2 caracteres es una tupla.
+**/
 function parseRecursively(block, long, pesoAnterior, blockTranslator, weightTranslator) {
     let miPeso = pesoAnterior;
     let miBlock = block.slice(-1 * long);
@@ -15,15 +21,10 @@ function parseRecursively(block, long, pesoAnterior, blockTranslator, weightTran
     let isTheLastBlock = isLastBlock(block, long);
     //end recurse when
     if (isTheLastBlock) {
-        return blockTranslator(miBlock, miPeso)+ ' ' + weightTranslator(miPeso) + ' ';
+        return blockTranslator(miBlock, miPeso);
     }
-
     miPeso = pesoAnterior++;
-    return parseRecursively (restante, long, miPeso + 1, blockTranslator, weightTranslator) + blockTranslator(miBlock,miPeso) + ' ' + weightTranslator(miPeso) + ' ';
-}
-
-function blockWeightTranslator(peso) {
-    return ["", "millones", "billones", "trillones", "cuatrillones", "quintillones"][peso];
+    return parseRecursively (restante, long, miPeso + 1, blockTranslator) + blockTranslator(miBlock,miPeso);
 }
 
 function isLastBlock(numberStr, long) {
